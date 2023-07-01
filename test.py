07fd79b7b4c80ccd00e8ccbe53e0dd75cd6b3614
BASIL AHAMED <sbasil.ahamed@gmail.com>
2023-07-01 15:36:38 +0530

    Create test.py
    main file

---
+++ test.py
@@ -0,0 +1,30 @@
+# main.py
+
+def greet(name):
+    """
+    A simple function that greets the user.
+    """
+    return f"Hello, {name}!"
+
+def add_numbers(a, b):
+    """
+    A function that adds two numbers.
+    """
+    return a + b
+
+def subtract_numbers(a, b):
+    """
+    A function that subtracts two numbers.
+    """
+    return a - b
+
+# Example usage
+name = input("Enter your name: ")
+greeting = greet(name)
+print(greeting)
+
+result = add_numbers(5, 3)
+print("Addition:", result)
+
+result = subtract_numbers(10, 4)
+print("Subtraction:", result)
